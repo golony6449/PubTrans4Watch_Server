7774d16fca092cea929c40b24fc85e9414976df0
Parkseongheum <ab6449@gmail.com>
2021-11-15 22:09:48 +0900

    역 도착정보 호출 API 작성

--- api/topis/topis.go
+++ api/topis/topis.go
@@ -1,9 +1,36 @@
 package PubTrans4Watch_Server
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func GetStationArrivalInfo(context *gin.Context) {
+	stationName := context.Param("station_name")
+	url := fmt.Sprintf("http://swopenAPI.seoul.go.kr/api/subway/%s/json/realtimeStationArrival/0/5/%s", os.Getenv("SECRET"), stationName)
+
+	if stationName == "" {
+		err := errors.New("empty parameter: station_name")
+		log.Fatal(err)
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//defer resp.Body.Close() // 모든 처리 후 Body Close 처리
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	context.JSON(200, gin.H{
-		"test": "test",
+		"data": string(data),
 	})
 }
